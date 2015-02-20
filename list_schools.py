28df06baebb5ed12eec7641cdce6276108f08cda
Ian Macalinao <me@ian.pw>
2015-02-19 22:34:25 -0600

    Modify script

--- list_schools.py
+++ list_schools.py
@@ -1,1 +1,3 @@
-print(sorted(set([email.split('@')[-1] for email in open('emails.txt').read().split('\n') if email.endswith('.edu')])))
+result = sorted(set([email.split('@')[-1] for email in open('emails.txt').read().split('\n') if email.endswith('.edu')]))
+print("Found " + str(len(result)) + " schools.")
+print('\n'.join(['  ' + r for r in result]))
