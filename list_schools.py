6be6f73fd4089fff0c009c79b56f0fb75eff86d6
Ian Macalinao <me@ian.pw>
2015-02-23 18:29:58 -0600

    Add total

--- list_schools.py
+++ list_schools.py
@@ -3,3 +3,4 @@ counts = [(domains.count(d), d) for d in set(domains)]
 counts.sort(reverse=True)
 print("Found " + str(len(counts)) + " schools.")
 print('\n'.join(['  ' + str(c[0]) + ' ' + str(c[1]) for c in counts]))
+print('Total: ' + str(sum([c[0] for c in counts])))
