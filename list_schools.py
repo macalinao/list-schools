6ab3ff3404e2a7a026575cf6424ca74c6cd1a3f9
Ian Macalinao <me@ian.pw>
2015-02-23 18:26:11 -0600

    Add counts

--- list_schools.py
+++ list_schools.py
@@ -1,3 +1,5 @@
-result = sorted(set([email.split('@')[-1] for email in open('emails.txt').read().split('\n') if email.endswith('.edu')]))
-print("Found " + str(len(result)) + " schools.")
-print('\n'.join(['  ' + r for r in result]))
+domains = [email.split('@')[-1] for email in open('emails.txt').read().split('\n') if email.endswith('.edu')]
+counts = [(domains.count(d), d) for d in set(domains)]
+counts.sort(reverse=True)
+print("Found " + str(len(counts)) + " schools.")
+print('\n'.join(['  ' + str(c[0]) + ' ' + str(c[1]) for c in counts]))
